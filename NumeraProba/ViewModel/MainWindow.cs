737c3a4d72a6b8fd2d7d87744d4fce02c369f8df
necotheone <ntho89@gmail.com>
2016-06-29 11:32:23 +0200

    Añadido expander

---
+++ NumeraProba/ViewModel/MainWindow.cs
@@ -0,0 +1,13 @@
+﻿using NumeraProba.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeraProba.ViewModel
+{
+    public class MainWindow : ObservableObject
+    {
+    }
+}
